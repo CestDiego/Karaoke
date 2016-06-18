488adc5128a5bde6fce878f5e9a1cbddc1f804c9
Giorgio Leveroni <giorgio.m.leveroni@gmail.com>
2016-06-18 00:27:54 -0400

    Firebase niceties

--- index.jsx
+++ index.jsx
@@ -1,8 +1,19 @@
-require("./node_modules/bootstrap/dist/css/bootstrap.min.css")
+import './node_modules/bootstrap/dist/css/bootstrap.min.css';
+import firebase from 'firebase';
+import ReactFireMixin from 'reactfire';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+firebase.initializeApp({
+  apiKey: 'AIzaSyAdlrCx_Rat0KoxYVO1PyCceQcvy9TgK30',
+  authDomain: 'karaoke-46318.firebaseapp.com',
+  databaseURL: 'https://karaoke-46318.firebaseio.com',
+  storageBucket: '',
+});
+
 export class App extends React.Component {
+  mixins: [ReactFireMixin]
+
   render() {
     return (
       <div>Simple React + Babel + Bootstrap + Webpack</div>
@@ -10,4 +21,4 @@ export class App extends React.Component {
   }
 }
 
-ReactDOM.render(<App/>, document.querySelector("#myApp"));
+ReactDOM.render(<App />, document.querySelector('#myApp'));
