683bab0aaa7deef2c64874c210c8db0b3ff5b7b4
Diego Berrocal <cestdiego@gmail.com>
2016-06-18 00:21:16 -0400

    Add Immutable JS and add song to queue function

--- src/lib/DemiQueue.js
+++ src/lib/DemiQueue.js
@@ -1,5 +1,20 @@
-const DemiQueue = {
+import Immutable from 'immutable'
+
+let DemiQueue = Immutable.Map({})
+
+let vote = (queue, url) {
 
 }
 
-export default DemiQueue
+let addEntity = (queue, url) {
+  if(queue.get(url) !== undefined) {
+    // TODO: Cannot repeat song, what do
+  }
+
+  return queue.set(url, {
+    url: url,
+    isCurrent: false,
+    votes: 0,
+    order: Object.keys(queue.toJS()).length,
+  })
+}
